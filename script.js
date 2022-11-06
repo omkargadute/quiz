ee40a160439ccac1a7a6157be7155aaa8fdaaa47
Omkar <omkargadute@gmail.com>
2022-11-06 12:14:16 +0530

    files added

---
+++ script.js
@@ -0,0 +1,11 @@
+var myArray = [
+    "Apples",
+    "Bananas",
+    "Pears"
+  ];
+  
+var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+
+function myFunction() {
+    alert(Hello);
+  }
